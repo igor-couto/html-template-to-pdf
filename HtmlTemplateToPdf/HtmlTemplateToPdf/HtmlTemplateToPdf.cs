093368a797d52031fba22c65435c42a9a68324d4
Igor Couto <igor.fcouto@gmail.com>
2020-09-13 23:28:03 -0300

    Project Upload

---
+++ HtmlTemplateToPdf/HtmlTemplateToPdf/HtmlTemplateToPdf.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace HtmlTemplateToPdf
+{
+    public class HtmlTemplateToPdf
+    {
+        public string Identifyer { private get; set; } = "@";
+
+        public PdfDocument GetPdf<T>(T model, string pathToHtml)
+        {
+            var properties = new Dictionary<string, string>();
+
+            foreach (var prop in model.GetType().GetProperties())
+                properties.Add(prop.Name, prop.GetValue(model, null).ToString());
+
+            var stringBuilder = new StringBuilder(File.ReadAllText(pathToHtml));
+
+            foreach (var property in properties)
+                stringBuilder.Replace(Identifyer + property.Key, property.Value);
+            
+            var bytes = OpenHtmlToPdf.Pdf.From(stringBuilder.ToString()).Content();
+
+            return new PdfDocument(bytes);
+        }
+
+        public void CreatePdf<T>(T model, string pathToHtml, string pathToPdf)
+        {
+            var pdf = GetPdf(model, pathToHtml);
+
+            using (var file = new FileStream(pathToPdf, FileMode.Create, FileAccess.Write))
+            {
+                file.Write(pdf.Bytes, 0, pdf.Bytes.Length);
+            }
+        }
+    }
+}
---
+++ HtmlTemplateToPdf/HtmlTemplateToPdf/PdfDocument.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace HtmlTemplateToPdf
+{
+    public class PdfDocument
+    {
+        public byte[] Bytes { get; }
+
+        public Stream Stream
+        {
+            get => new MemoryStream(Bytes);
+        }
+
+        public PdfDocument(byte[] bytes)
+            => Bytes = bytes;
+    }
+}
